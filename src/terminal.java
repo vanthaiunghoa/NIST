8fb4a1fcbb55bf90cfa4e361ac237370c8a5f151
yogendra saxena <yogendrasaxena56@gmail.com>
2016-07-27 14:04:08 +0530

    created common copy folder after main

--- src/terminal.java
+++ src/terminal.java
@@ -28,11 +28,7 @@ class terminal
 			            if (line == null) { break; }
 			            System.out.println(line2);
 			        }
-			        
-			        //C:\\nbis_v5_0_0\\Rel_5.0.0\\mindtct\\bin\\output1.xyt
-			        //C:\\nbis_v5_0_0\\Rel_5.0.0\\bozorth3\\bin\\
-			         
-		
+			      
 		//-----------------------------------------------------------------
 		File f = null;
 	      boolean bool = false;
@@ -69,94 +65,85 @@ class terminal
 	      //-------------------------------------------------------------
 	      
 	      File srcFolder = new File("c:\\nbis_v5_0_0\\Rel_5.0.0\\mindtct\\bin\\output1.xyt");
-	    	File destFolder = new File("c:\\nbis_v5_0_0\\Rel_5.0.0\\bozorth3\\bin\\output1.xyt");
+	      File destFolder = new File("c:\\nbis_v5_0_0\\Rel_5.0.0\\bozorth3\\bin\\output1.xyt");
 	    	
-	    	if(!srcFolder.exists()){
+	    	if(!srcFolder.exists())
+	    	{
 
 	           System.out.println("Directory does not exist.");
 	           //just exit
 	           System.exit(0);
 
-	        }else{
+	        }
+	    	else
+	    	{
 
-	           try{
+	           try
+	           {
 	        	copyFolder(srcFolder,destFolder);
-	           }catch(IOException e){
+	           }
+	           catch(IOException e)
+	           {
 	        	e.printStackTrace();
 	        	//error, just exit
 	                System.exit(0);
 	           }
-	        }
-	    	System.out.println("Done");
-	    }
-	
-	    public static void copyFolder(File src, File dest)
-	    	throws IOException{
-	    	
-	    	if(src.isDirectory()){
-	    		
-	    		//if directory not exists, create it
-	    		if(!dest.exists()){
-	    		   dest.mkdir();
-	    		   System.out.println("Directory copied from " 
-	                              + src + "  to " + dest);
-	    		}
-	    		
-	    		
-	    		String files[] = src.list();
-	    		
-	    		for (String file : files) {
-	    		  
-	    		   File srcFile = new File(src, file);
-	    		   File destFile = new File(dest, file);
-	    		  
-	    		   copyFolder(srcFile,destFile);
-	    		}
-	    	   
-	    	}else{
-	    		
-	    		InputStream in = new FileInputStream(src);
-	    	        OutputStream out = new FileOutputStream(dest); 
-	    	                     
-	    	        byte[] buffer = new byte[1024];
-	    	    
-	    	        int length;
-	    	       
-	    	        while ((length = in.read(buffer)) > 0){
-	    	    	   out.write(buffer, 0, length);
-	    	        }
-	 
-	    	        in.close();
-	    	        out.close();
-	    	        System.out.println("File copied from " + src + " to " + dest);
-	    	}
-    	
-    	//-------------------------------------------------------------------
-	    	 File srcFolder = new File("c:\\nbis_v5_0_0\\Rel_5.0.0\\mindtct\\bin\\output2.xyt");
-		    	File destFolder = new File("c:\\nbis_v5_0_0\\Rel_5.0.0\\bozorth3\\bin\\output2.xyt");
-	    	
-	    	
-	    	if(!srcFolder.exists()){
-
-	           System.out.println("Directory does not exist.");
-	          
-	           System.exit(0);
-
-	        }else{
-
-	           try{
-	        	copyFolder2(srcFolder,destFolder);
-	           }catch(IOException e){
-	        	e.printStackTrace();
 	        
-	                System.exit(0);
-	           }
-	        }
+	    	System.out.println("Done");
+	    	}
 	    
-	    }
+    	//-------------------------------------------------------------------
+	    	 File srcFolder1 = new File("c:\\nbis_v5_0_0\\Rel_5.0.0\\mindtct\\bin\\output2.xyt");
+		    	File destFolder1 = new File("c:\\nbis_v5_0_0\\Rel_5.0.0\\bozorth3\\bin\\output2.xyt");
+	    	
+		    	if(!srcFolder1.exists()){
+
+			           System.out.println("Directory does not exist.");
+			           //just exit
+			           System.exit(0);
+
+			        }else{
+
+			           try{
+			        	copyFolder(srcFolder1,destFolder1);
+			           }catch(IOException e){
+			        	e.printStackTrace();
+			        	//error, just exit
+			                System.exit(0);
+			           }
+			        
+			    	System.out.println("Done");
+			    }
+	    	
+    	
+		//-------------------------------------------------------------------	        
+			    System.out.print("Minuatea rate is ");   
+			      ProcessBuilder builder3 = new ProcessBuilder(
+				            "cmd.exe", "/c", "cd \"C:\\nbis_v5_0_0\\Rel_5.0.0\\bozorth3\\bin\" && bozorth3 output1.xyt output2.xyt");
+				        builder3.redirectErrorStream(true);
+				        Process p3 = builder3.start();
+				        BufferedReader r3 = new BufferedReader(new InputStreamReader(p3.getInputStream()));
+				        String line3;
+				        while (true) {
+				            line3 = r3.readLine();
+				            if (line3 == null) { break; }
+				            System.out.println(line3);
+				            if(Integer.parseInt(line3) == 494)
+					        	System.out.println("Finger Print Matched");
+					        else if(Integer.parseInt(line3) == 0)
+					        	System.out.println("Finger Print Not Matched");
+					        else
+					        	System.out.println("Finger Print Partially Matched");
+				        }
+				        
+				        	
+           
+	    	}   
 	    
-	    public static void copyFolder2(File src, File dest)
-	    	throws IOException{
+	  
+	    public static void copyFolder(File src, File dest)
+	    	throws IOException
+	    	{
 	    	
 	    	if(src.isDirectory()){
 	    		
@@ -194,29 +181,5 @@ class terminal
 	    	        out.close();
 	    	        System.out.println("File copied from " + src + " to " + dest);
 	    	}
-    	
-		//-------------------------------------------------------------------	        
-			    System.out.print("Minuatea rate is ");   
-			      ProcessBuilder builder3 = new ProcessBuilder(
-				            "cmd.exe", "/c", "cd \"C:\\nbis_v5_0_0\\Rel_5.0.0\\bozorth3\\bin\" && bozorth3 output1.xyt output2.xyt");
-				        builder3.redirectErrorStream(true);
-				        Process p3 = builder3.start();
-				        BufferedReader r3 = new BufferedReader(new InputStreamReader(p3.getInputStream()));
-				        String line3;
-				        while (true) {
-				            line3 = r3.readLine();
-				            if (line3 == null) { break; }
-				            System.out.println(line3);
-				            if(Integer.parseInt(line3) == 494)
-					        	System.out.println("Finger Print Matched");
-					        else if(Integer.parseInt(line3) == 0)
-					        	System.out.println("Finger Print Not Matched");
-					        else
-					        	System.out.println("Finger Print Partially Matched");
-				        }
-				        
-				        	
-           
-}       
-		
 	}
+}
